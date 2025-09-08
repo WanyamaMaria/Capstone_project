839340734c40773ecc00bffcf6f30660edce042a
Arinda Alvin Aaron <157710138+Infinite-7@users.noreply.github.com>
2025-09-08 09:53:19 +0300

    Update Service.php

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+   // Allow mass assignment for these fields
+    protected $fillable = [
+        'name',
+        'description',
+        'facility_id',
+    ];
+
+    // Service belongs to a Facility
+    public function facility()
+    {
+        return $this->belongsTo(Facility::class);
+    } //
 }
